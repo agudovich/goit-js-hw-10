420b3a467c4d6fb92644f59def1b673439ad43da
Gudovich <a@11.je>
2025-06-23 16:01:05 +0300

    fix img

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -59,7 +59,7 @@ iziToast.settings({
   close: true,
   color: '#ef4040',
   progressBarColor: '#b51b1b',
-  iconUrl: '${import.meta.env.BASE_URL}img/bi_x-octagon.svg',
+  iconUrl: '/goit-js-hw-10/img/bi_x-octagon.svg',
   iconColor: '#fff',
   transitionIn: 'fadeInDown',
 });
